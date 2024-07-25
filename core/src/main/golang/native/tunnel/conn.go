f45c0347a46f8ce5a9bf7211d53e22990ad55418
forestL <45709305+ForestL18@users.noreply.github.com>
2024-07-26 04:13:44 +0800

    Update conn.go

--- core/src/main/golang/native/tunnel/conn.go
+++ core/src/main/golang/native/tunnel/conn.go
@@ -12,32 +12,26 @@ func CloseAllConnections() {
 	})
 }
 
-func closeMatch(filter func(conn C.Connection) bool) {
+func closeMatch(filter func(conn C.Conn) bool) {
 	statistic.DefaultManager.Range(func(c statistic.Tracker) bool {
 		if cc, ok := c.(C.Conn); ok {
 			if filter(cc) {
-				_ = c.Close()
+				_ = cc.Close()
 				return true
 			}
 		}
-		if cc, ok := c.(C.PacketConn); ok {
-			if filter(cc) {
-				_ = c.Close()
-				return true
-			}
-		}
-		return true
+		return false
 	})
 }
 
 func closeConnByGroup(name string) {
-	closeMatch(func(conn C.Connection) bool {
+	closeMatch(func(conn C.Conn) bool {
 		for _, c := range conn.Chains() {
 			if c == name {
 				return true
 			}
 		}
-		
+
 		return false
 	})
 }
