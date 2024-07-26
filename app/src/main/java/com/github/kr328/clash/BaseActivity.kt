13b047b3fc991aeb108d0539110f68617ab1ef73
forestL <45709305+ForestL18@users.noreply.github.com>
2024-07-26 13:01:55 +0800

    Update BaseActivity.kt

--- app/src/main/java/com/github/kr328/clash/BaseActivity.kt
+++ app/src/main/java/com/github/kr328/clash/BaseActivity.kt
@@ -29,18 +29,32 @@ import java.util.concurrent.atomic.AtomicInteger
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
-abstract class BaseActivity<D : Design<*>> : AppCompatActivity(),
+abstract class BaseActivity<D : Design<*>> :
+    AppCompatActivity(),
     CoroutineScope by MainScope(),
     Broadcasts.Observer {
-    
+    enum class Event {
+        ServiceRecreated,
+        ActivityStart,
+        ActivityStop,
+        ClashStop,
+        ClashStart,
+        ProfileLoaded,
+        ProfileChanged,
+        ProfileUpdateCompleted,
+        ProfileUpdateFailed
+    }
+
+
     protected val uiStore by lazy { UiStore(this) }
     protected val events = Channel<Event>(Channel.UNLIMITED)
     protected var activityStarted: Boolean = false
     protected val clashRunning: Boolean
         get() = Remote.broadcasts.clashRunning
     protected var design: D? = null
-        set(value) {
+        private set(value) {
             field = value
+
             if (value != null) {
                 setContentView(value.root)
             } else {
@@ -68,7 +82,7 @@ abstract class BaseActivity<D : Design<*>> : AppCompatActivity(),
         ActivityResultLifecycle().use { lifecycle, start ->
             suspendCoroutine { c ->
                 activityResultRegistry.register(requestKey, lifecycle, contracts) {
-                    c.resume(it)
+                    c.resumeWith(Result.success(it))
                 }.apply { start() }.launch(input)
             }
         }
@@ -78,6 +92,7 @@ abstract class BaseActivity<D : Design<*>> : AppCompatActivity(),
         suspendCoroutine<Unit> {
             window.decorView.post {
                 this.design = design
+
                 it.resume(Unit)
             }
         }
@@ -85,35 +100,49 @@ abstract class BaseActivity<D : Design<*>> : AppCompatActivity(),
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         applyDayNight()
 
         launch {
             main()
+
+            finish()
         }
     }
 
     override fun onStart() {
         super.onStart()
+
         activityStarted = true
+
         Remote.broadcasts.addObserver(this)
+
         events.trySend(Event.ActivityStart)
     }
 
     override fun onStop() {
         super.onStop()
+
         activityStarted = false
+
         Remote.broadcasts.removeObserver(this)
+
         events.trySend(Event.ActivityStop)
     }
 
     override fun onDestroy() {
         design?.cancel()
+
         cancel()
+
         super.onDestroy()
     }
 
     override fun finish() {
-        if (deferRunning) return
+        if (deferRunning) {
+            return
+        }
+
         deferRunning = true
 
         launch {
@@ -143,6 +172,7 @@ abstract class BaseActivity<D : Design<*>> : AppCompatActivity(),
 
     override fun onSupportNavigateUp(): Boolean {
         this.onBackPressed()
+
         return true
     }
 
@@ -172,7 +202,6 @@ abstract class BaseActivity<D : Design<*>> : AppCompatActivity(),
 
     override fun onStopped(cause: String?) {
         events.trySend(Event.ClashStop)
-
         if (cause != null && activityStarted) {
             launch {
                 design?.showExceptionToast(ClashException(cause))
@@ -182,45 +211,50 @@ abstract class BaseActivity<D : Design<*>> : AppCompatActivity(),
 
     private fun queryDayNight(config: Configuration = resources.configuration): DayNight {
         return when (uiStore.darkMode) {
-            DarkMode.Auto -> if (config.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES) DayNight.Night else DayNight.Day
-            DarkMode.ForceLight -> DayNight.Day
-            DarkMode.ForceDark -> DayNight.Night
+            DarkMode.Auto -> {
+
+                if (config.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES)
+                    DayNight.Night
+                else
+                    DayNight.Day
+            }
+            DarkMode.ForceLight -> {
+                DayNight.Day
+            }
+            DarkMode.ForceDark -> {
+                DayNight.Night
+            }
         }
     }
 
     private fun applyDayNight(config: Configuration = resources.configuration) {
         val dayNight = queryDayNight(config)
+
         when (dayNight) {
-            DayNight.Night -> theme.applyStyle(R.style.AppThemeDark, true)
-            DayNight.Day -> theme.applyStyle(R.style.AppThemeLight, true)
+            DayNight.Night -> {
+                theme.applyStyle(R.style.AppThemeDark, true)
+            }
+            DayNight.Day -> {
+                theme.applyStyle(R.style.AppThemeLight, true)
+            }
         }
 
         window.isAllowForceDarkCompat = false
         window.isSystemBarsTranslucentCompat = true
-        
+
         window.statusBarColor = resolveThemedColor(android.R.attr.statusBarColor)
         window.navigationBarColor = resolveThemedColor(android.R.attr.navigationBarColor)
 
         if (Build.VERSION.SDK_INT >= 23) {
-            window.isLightStatusBarsCompat = resolveThemedBoolean(android.R.attr.windowLightStatusBar)
+            window.isLightStatusBarsCompat =
+                resolveThemedBoolean(android.R.attr.windowLightStatusBar)
         }
 
         if (Build.VERSION.SDK_INT >= 27) {
-            window.isLightNavigationBarCompat = resolveThemedBoolean(android.R.attr.windowLightNavigationBar)
+            window.isLightNavigationBarCompat =
+                resolveThemedBoolean(android.R.attr.windowLightNavigationBar)
         }
 
         this.dayNight = dayNight
-    }
-
-    enum class Event {
-        ServiceRecreated,
-        ActivityStart,
-        ActivityStop,
-        ClashStop,
-        ClashStart,
-        ProfileLoaded,
-        ProfileChanged,
-        ProfileUpdateCompleted,
-        ProfileUpdateFailed,
     }
 }
