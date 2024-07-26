4e05c19c6ef81b0f512ba61f4e07751c5a072bd0
forestL <45709305+ForestL18@users.noreply.github.com>
2024-07-26 11:27:33 +0800

    Update conn.go

--- core/src/main/golang/native/tunnel/conn.go
+++ core/src/main/golang/native/tunnel/conn.go
@@ -1,10 +1,8 @@
 package tunnel
-
 import (
 	C "github.com/metacubex/mihomo/constant"
 	"github.com/metacubex/mihomo/tunnel/statistic"
 )
-
 func CloseAllConnections() {
 	statistic.DefaultManager.Range(func(c statistic.Tracker) bool {
 		_ = c.Close()
@@ -12,26 +10,32 @@ func CloseAllConnections() {
 	})
 }
 
-func closeMatch(filter func(conn C.Conn) bool) {
+func closeMatch(filter func(conn C.Connection) bool) {
 	statistic.DefaultManager.Range(func(c statistic.Tracker) bool {
 		if cc, ok := c.(C.Conn); ok {
 			if filter(cc) {
-				_ = cc.Close()
+				_ = c.Close()
 				return true
 			}
 		}
-		return false
+		if cc, ok := c.(C.PacketConn); ok {
+			if filter(cc) {
+				_ = c.Close()
+				return true
+			}
+		}
+		return true
 	})
 }
 
 func closeConnByGroup(name string) {
-	closeMatch(func(conn C.Conn) bool {
+	closeMatch(func(conn C.Connection) bool {
 		for _, c := range conn.Chains() {
 			if c == name {
 				return true
 			}
 		}
-
+		
 		return false
 	})
 }
