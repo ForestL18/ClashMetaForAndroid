1f31de3cf3784f9a98708a2cd0b9a9e81b025a7a
ForestL <45709305+ForestL18@users.noreply.github.com>
2024-08-23 01:53:18 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,10 +35,10 @@ val geoFilesDownloadDir = "src/main/assets"
 task("downloadGeoFiles") {
 
     val geoFilesUrls = mapOf(
-        "https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geoip.dat" to "geoip.dat",
+        "https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geoip.metadb" to "geoip.metadb",
         "https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geosite.dat" to "geosite.dat",
-        "https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/country.mmdb" to "country.mmdb",
-        "https://github.com/P3TERX/GeoLite.mmdb/raw/download/GeoLite2-ASN.mmdb" to "ASN.mmdb",
+        // "https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/country.mmdb" to "country.mmdb",
+        // "https://github.com/P3TERX/GeoLite.mmdb/raw/download/GeoLite2-ASN.mmdb" to "ASN.mmdb",
     )
 
     doLast {
